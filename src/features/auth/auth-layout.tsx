06e2c96d57b2d61c6b48f8ff95b3ffe16165da96
Joseph Cecil Odoi <josephgodwins015@gmail.com>
2025-01-14 16:46:41 +0000

    Fixed img on authLayout

--- src/features/auth/auth-layout.tsx
+++ src/features/auth/auth-layout.tsx
@@ -6,7 +6,7 @@ export default function AuthLayout({ children }: Props) {
   return (
     <div className="container grid h-screen flex-col items-center justify-center bg-primary-foreground lg:max-w-none lg:px-0">
       <div className="mx-auto flex w-full flex-col justify-center space-y-4 sm:w-[480px] lg:p-8">
-        <div className="mb-3 flex items-center justify-center">
+        <div className="-ml-10 mb-3 flex items-center justify-center">
           
           <img 
             src="/Cocobod Logo-01.png" 
