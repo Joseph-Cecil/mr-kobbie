109a6955a2916e73f24e1f47446b86f99a304362
Joseph Cecil Odoi <88081064+Joseph-Cecil@users.noreply.github.com>
2025-03-07 09:59:10 +0000

    Search functionality corrected

--- src/features/admin/components/data-table-toolbar.tsx
+++ src/features/admin/components/data-table-toolbar.tsx
@@ -19,12 +19,12 @@ export function DataTableToolbar<TData>({
     <div className='flex items-center justify-between'>
       <div className='flex flex-1 flex-col-reverse items-start gap-y-2 sm:flex-row sm:items-center sm:space-x-2'>
         <Input
-          placeholder='Filter users  by staffId...'
+          placeholder='Filter users  by firstName...'
           value={
-            (table.getColumn('staffId')?.getFilterValue() as string) ?? ''
+            (table.getColumn('firstName')?.getFilterValue() as string) ?? ''
           }
           onChange={(event) =>
-            table.getColumn('staffId')?.setFilterValue(event.target.value)
+            table.getColumn('firstName')?.setFilterValue(event.target.value)
           }
           className='h-8 w-[150px] lg:w-[250px]'
         />
--- src/features/contributionDashboard/index.tsx
+++ src/features/contributionDashboard/index.tsx
@@ -6,7 +6,6 @@ import { Search } from "@/components/search";
 import { ThemeSwitch } from "@/components/theme-switch";
 import { columns } from "./components/users-columns";
 import { UsersDialogs } from "./components/users-dialogs";
-import { UsersPrimaryButtons } from "./components/users-primary-buttons";
 import { UsersTable } from "./components/users-table";
 import UsersProvider from "./context/users-context";
 import { userListSchema, User } from "./data/schema";
@@ -64,7 +63,6 @@ export default function ContributionDashboard() {
               Search and View Staff Contributions Here.
             </p>
           </div>
-          <UsersPrimaryButtons />
         </div>
         <div className="-mx-4 flex-1 overflow-auto px-4 py-1 lg:flex-row lg:space-x-12 lg:space-y-0">
           <UsersTable data={users} columns={columns} />
